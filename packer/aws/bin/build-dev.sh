316582b7cdd0354cdf4e0923d4097065f8701086
xwang2713 <xiaoming.wang@lexisnexis.com>
2023-01-23 07:17:54 -0500

    Update top build-dev.sh

--- packer/aws/bin/build-dev.sh
+++ packer/aws/bin/build-dev.sh
@@ -28,6 +28,24 @@ export SSH_PRIVATE_KEY_FILE=~/.ssh/hpcc-build
 #../cloud-image-build/packer/aws/build-dev.sh -c amzn2 -D xvda -a x86_64 -u ec2-user -r ca-central-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
 #../cloud-image-build/packer/aws/build-dev.sh -c el7 -a x86_64 -u centos -r ca-central-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
 #../cloud-image-build/packer/aws/build-dev.sh -c el7 -f j11 -a x86_64 -T t2.large -u centos -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
+
+#../cloud-image-build/packer/aws/build-dev.sh -c el7 -f j11mysql56 -a x86_64 -T t2.large -u centos -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
+
+../cloud-image-build/packer/aws/build-dev.sh -c el7 -f mysql57 -a x86_64 -T t2.large -u centos -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
+#ami-01a9774157c7e995e
+
+#../cloud-image-build/packer/aws/build-dev.sh -c el7 -f mysql8 -a x86_64 -T t2.large -u centos -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
+#ami-007c4fa7d4b0aabed
+
 #../cloud-image-build/packer/aws/build-dev.sh -c el7 -f 8x -a x86_64 -T t2.large -u centos -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
-../cloud-image-build/packer/aws/build-dev.sh -c amzn2 -f j11 -D xvda -a x86_64  -T t2.large -u ec2-user -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
+
+#../cloud-image-build/packer/aws/build-dev.sh -c el7 -f 8xmysql8 -a x86_64 -T t2.large -u centos -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
+#ami-0fae8f7b0bb90ec5c
+
+
+# -f 8x0 will use mysqlclient 5.6.44-1
+#../cloud-image-build/packer/aws/build-dev.sh -c el7 -f 8x0 -a x86_64 -T t2.large -u centos -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
+#../cloud-image-build/packer/aws/build-dev.sh -c amzn2 -f j11 -D xvda -a x86_64  -T t2.large -u ec2-user -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
 #../cloud-image-build/packer/aws/build-dev.sh -c amzn2 -f 8x -D xvda -a x86_64  -T t2.large -u ec2-user -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
+#../cloud-image-build/packer/aws/build-dev.sh -c focal -T t2.large -f 8x -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
+#../cloud-image-build/packer/aws/build-dev.sh -c el8s -f 8x -a x86_64 -T t2.large -u centos -r us-east-1 -p subnet-ids-ris -P icanspellthis | tee -a build.log 2>&1
